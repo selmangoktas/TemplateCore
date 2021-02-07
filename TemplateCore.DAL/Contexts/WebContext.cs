bb4b8bc6d23f316bb4c46aaf00310df8a8ec7169
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2021-02-07 04:05:28 +0300

    akaterımlar

---
+++ TemplateCore.DAL/Contexts/WebContext.cs
@@ -0,0 +1,28 @@
+﻿using TemplateCore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateCore.DAL.Contexts
+{
+    public class WebContext : DbContext
+    {
+        public WebContext(DbContextOptions<WebContext> opt) : base(opt)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Test>().HasKey(x => x.name);
+            //modelBuilder.Entity<Product>().HasOne(o => o.Brand).WithMany(w => w.Products).OnDelete(DeleteBehavior.SetNull);
+
+            //modelBuilder.Entity<ProductCategory>().HasKey(hk => new { hk.ProductID, hk.CategoryID });
+
+            ////SEED DATA
+            //modelBuilder.Entity<Admin>().HasData(new Admin { EmailAddress = "slmngoktas@gmail.com", ID = 1, Name = "Selman", Surname = "GÖKTAŞ", Password = "123456" });
+        }
+        public DbSet<Test> Test { get; set; }
+
+       
+       
+    }
+}
---
+++ TemplateCore.DAL/Entities/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateCore.DAL.Entities
+{
+    public class Test
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
