4c3eda5a2b4350eb0d86309717b4ea5cd955de85
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2021-02-07 03:42:33 +0300

    ilk yukleme

---
+++ TemplateCore.BL/Repositories/WebRepository.cs
@@ -0,0 +1,73 @@
+﻿using TemplateCore.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TemplateCore.BL
+{
+    public class WebRepository<T> where T : class
+    {
+        private readonly WebContext db;
+        private DbSet<T> entities;
+
+        public WebRepository(WebContext _db)
+        {
+            db = _db;
+            entities = _db.Set<T>();
+        }
+
+        public T GetBy(Expression<Func<T, bool>> expression)
+        {
+            return entities.FirstOrDefault(expression);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return entities;
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return entities.Where(expression);
+        }
+
+        public void Add(T entity)
+        {
+            db.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void AddRange(IQueryable<T> entities)
+        {
+            db.AddRange(entities);
+            db.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            db.Remove(entity);
+            db.SaveChanges();
+        }
+
+        public void RemoveRange(IQueryable<T> entities)
+        {
+            db.RemoveRange(entities);
+            db.SaveChanges();
+        }
+
+        public void Update(T entity, params Expression<Func<T, object>>[] expressions)
+        {
+            if (expressions.Any()) foreach (Expression<Func<T, object>> expression in expressions) db.Entry(entity).Property(expression).IsModified = true;
+            else db.Update(entity);
+            db.SaveChanges();
+        }
+
+        //public void Update(T entity)
+        //{
+        //    db.Update(entity);
+        //    db.SaveChanges();
+        //}
+    }
+}
+
